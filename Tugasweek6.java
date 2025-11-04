f1f0fef709affb7c66a1741e6621b8b72cd32b4e
keisyaavriliahumaira88-ops <keisyaavriliahumaira88@gmail.com>
2025-11-04 05:05:59 -0800

    membuat variabel

--- Tugasweek6.java
+++ Tugasweek6.java
@@ -4,6 +4,15 @@ public class Tugasweek6{
     public static void main(String[] args) {
        
       Scanner ns = new Scanner(System.in);
-      
+
+        double strukturData, pbo, basisData, algoritma, rpl;
+        double rataRata, ipk;
+        String predikat;
+
+       
+
+
+
+
     }
 }
