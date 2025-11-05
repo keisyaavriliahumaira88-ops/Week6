057065b4c2b0f54dc60fdd04e906c02dd81122be
keisyaavriliahumaira88-ops <keisyaavriliahumaira88@gmail.com>
2025-11-04 20:39:27 -0800

    tampilkan daftar mata kuliah yg di ajukan dan cek nilai pd matkul ygt di pilih

--- Tugasweek6.java
+++ Tugasweek6.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Tugasweek6{
     public static void main(String[] args) {
        
-      Scanner ns = new Scanner(System.in);
+      Scanner input = new Scanner(System.in);
 
         double strukturData, pbo, basisData, algoritma, rpl;
         double rataRata, ipk;
@@ -11,28 +11,28 @@ public class Tugasweek6{
 
         System.out.println("SELEKSI ASISTEN DOSEN");
         System.out.print("Masukkan nilai Struktur Data: ");
-        strukturData = ns.nextDouble();
+        strukturData = input.nextDouble();
         System.out.print("Masukkan nilai Pemrograman Berorientasi Objek: ");
-        pbo = ns.nextDouble();
+        pbo = input.nextDouble();
         System.out.print("Masukkan nilai Basis Data: ");
-        basisData = ns.nextDouble();
+        basisData = input.nextDouble();
         System.out.print("Masukkan nilai Algoritma dan Pemrograman: ");
-        algoritma = ns.nextDouble();
+        algoritma = input.nextDouble();
         System.out.print("Masukkan nilai Rekayasa Perangkat Lunak: ");
-        rpl = ns.nextDouble();
+        rpl = input.nextDouble();
         
         rataRata = (strukturData + pbo + basisData + algoritma + rpl) / 5;
         ipk = (rataRata / 100) * 4;
 
         if (ipk >= 3.75) {
             predikat = "A (Sangat Baik)";
-        } else if (ipk >= 3.50) {
+          } else if (ipk >= 3.50) {
             predikat = "AB (Baik Sekali)";
-        } else if (ipk >= 3.00) {
+          } else if (ipk >= 3.00) {
             predikat = "B (Baik)";
-        } else if (ipk >= 2.50) {
+          } else if (ipk >= 2.50) {
             predikat = "BC (Cukup)";
-        } else {
+          } else {
             predikat = "C (Kurang)";
         }
         System.out.println("\nRata-rata nilai: " + String.format("%.2f", rataRata));
@@ -44,19 +44,48 @@ public class Tugasweek6{
         System.out.println("Minimal predikat A atau AB.");
         System.exit(0); // Program selesai
         }
+        System.out.println("\nPilih mata kuliah yang ingin diajukan sebagai Asisten Dosen:");
+        System.out.println("1. Struktur Data");
+        System.out.println("2. Pemrograman Berorientasi Objek");
+        System.out.println("3. Basis Data");
+        System.out.println("4. Algoritma dan Pemrograman");
+        System.out.println("5. Rekayasa Perangkat Lunak");
+        System.out.print("Masukkan pilihan (1-5): ");
+        int pilihan = input.nextInt();
+        double nilaiPilihan = 0;
+        String namaMatkul = "";
 
-
-
-
-
-        
-
-
-
-       
-
-
-
-
-    }
+        switch (pilihan) {
+            case 1:
+                nilaiPilihan = strukturData;
+                namaMatkul = "Struktur Data";
+                break;
+            case 2:
+                nilaiPilihan = pbo;
+                namaMatkul = "Pemrograman Berorientasi Objek";
+                break;
+            case 3:
+                nilaiPilihan = basisData;
+                namaMatkul = "Basis Data";
+                break;
+            case 4:
+                nilaiPilihan = algoritma;
+                namaMatkul = "Algoritma dan Pemrograman";
+                break;
+            case 5:
+                nilaiPilihan = rpl;
+                namaMatkul = "Rekayasa Perangkat Lunak";
+                break;
+            default:
+                System.out.println("Pilihan tidak valid.");
+                System.exit(0);
+        }
+        if (nilaiPilihan >= 75) {
+            System.out.println("\nSelamat! Anda dinyatakan LULUS seleksi Asisten Dosen untuk mata kuliah " + namaMatkul + ".");
+          } else {
+            System.out.println("\nMaaf, nilai Anda pada mata kuliah " + namaMatkul +
+                    " belum memenuhi syarat minimal (75).");
+        }
+        input.close();
+      }
 }
