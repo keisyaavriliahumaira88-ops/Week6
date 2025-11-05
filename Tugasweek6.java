5ed9be09b30094bba75a000fb8b3313f7f805bbe
keisyaavriliahumaira88-ops <keisyaavriliahumaira88@gmail.com>
2025-11-04 19:28:41 -0800

    menghitung nilai rata-rata

--- Tugasweek6.java
+++ Tugasweek6.java
@@ -9,8 +9,22 @@ public class Tugasweek6{
         double rataRata, ipk;
         String predikat;
 
-        System.out.println("=== SELEKSI ASISTEN DOSEN ===");
+        System.out.println("SELEKSI ASISTEN DOSEN");
+        System.out.print("Masukkan nilai Struktur Data: ");
+        strukturData = ns.nextDouble();
+        System.out.print("Masukkan nilai Pemrograman Berorientasi Objek: ");
+        pbo = ns.nextDouble();
+        System.out.print("Masukkan nilai Basis Data: ");
+        basisData = ns.nextDouble();
+        System.out.print("Masukkan nilai Algoritma dan Pemrograman: ");
+        algoritma = ns.nextDouble();
+        System.out.print("Masukkan nilai Rekayasa Perangkat Lunak: ");
+        rpl = ns.nextDouble();
         
+        rataRata = (strukturData + pbo + basisData + algoritma + rpl) / 5;
+
+        
+
 
 
        
